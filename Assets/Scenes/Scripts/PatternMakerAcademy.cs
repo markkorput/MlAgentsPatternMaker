76215843b2cfe98f834c2da0fe445d764a39a604
mark <mark.korput@gmail.com>
2019-06-26 13:29:58 +0200

    Starting to form Academy and Agent classes and nt classes and scene layout

---
+++ Assets/Scenes/Scripts/PatternMakerAcademy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+namespace PatternMaker {
+    public class PatternMakerAcademy : Academy
+    {
+        private int PatternWidth;
+        private int PatternHeight;
+
+        public override void InitializeAcademy() {
+            PatternWidth = (int)base.resetParameters["PatternWidth"];
+            PatternWidth = (int)base.resetParameters["PatternHeight"];
+        }
+
+        public override void AcademyReset() {
+
+        }
+
+        public override void AcademyStep() {
+
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/PatternMakerAgent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+namespace PatternMaker {
+    public class PatternMakerAgent : Agent
+    {
+
+    }
+}
